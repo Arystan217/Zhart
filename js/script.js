d30eecb07328a44cca8055ce8c27e7dc8136bd5e
Arystan217 <65996409+Arystan217@users.noreply.github.com>
2021-08-08 20:52:23 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+  $('.burger-icon').click(function(){
+    $(this).toggleClass('burger-icon--active');
+    $('.burger').toggleClass('burger--active')
+    $("header").toggleClass("header-burger-open");
+  });
+
+  if (window.scrollY > 0){
+    $("header").addClass("header--scroll");
+  }
+  else {
+    $("header").removeClass("header--scroll");
+  }
+  // шапка при скролле
+  $(window).scroll(function(){
+    if (this.scrollY > 0){
+      $("header").addClass("header--scroll");
+    }
+    else {
+      $("header").removeClass("header--scroll");
+    }
+  });
+
+  // плавная прокрутка
+  $('.nav__link').on('click', function(){
+    scrollTo = $(this).attr('data-scroll-path');
+    $('html,body').stop().animate({ scrollTop: $('#' + scrollTo).offset().top - 95}, 880);
+    if ($(this).attr('data-scroll-path') == 'hero') {
+      scrollTo = $(this).attr('data-scroll-path');
+      $('html,body').stop().animate({ scrollTop: $('#' + scrollTo).offset().top - 180}, 880);
+    }
+  });
+  $('.burger__link').on('click', function(){
+    $('.burger-icon').removeClass('burger-icon--active');
+    $('.burger').removeClass('burger--active');
+    $('header').removeClass('header-burger-open');
+  });
+
+  $('#phone').inputmask('+7-(999)-999-99-99',{ showMaskOnHover: false });
+
+
+
+});
